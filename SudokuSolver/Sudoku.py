90a33662a944223c66ef1c0cf2e05a68ff42f887
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-01-26 15:56:24 +0100

    Validity and explore functions

--- SudokuSolver/Sudoku.py
+++ SudokuSolver/Sudoku.py
@@ -9,38 +9,56 @@ class Sudoku:
     def __init__ (self):
         """Initializes the sudoku"""
         self.isComplet = False
+        self.emptyFrame = 0
         self.matrix = {}
-        self.MAX = 9
         
-        for i in range(self.MAX):
-            for j in range(self.MAX):
+        for i in range(9):
+            for j in range(9):
                 randomf = random.choice(range(100)) % 10
                 self.matrix[i,j] = randomf
     
-    def loadContent (self):
+    def loadContent2 (self):
         """Loads sudoku data"""
-        #for i in range(self.MAX):
-        #    for j in range(self.MAX):
+        #for i in range(9):
+        #    for j in range(9):
         #        randomf = random.choice(range(100)) % 10
         #        self.matrix[i,j] = randomf
 
         #fillDefaultMatrix(self.matrix)
+        a = 1
+
+    def loadContent (self):
+        """Loads sudoku data"""
         fillPersoMatrix(self.matrix)
+
+    def solve(self):
+        self.emptyFrame = emptyFrameNumber()
+        #while( not self.isComplet):
+        a = 1
 
     # =======================================================================================
     # Explore Functions ======================================================================
     # =======================================================================================
 
+    def emptyFrameNumber (self):
+        """Return the number of empty frame in the matrix"""
+        count = 0
+        for i in range (9):
+            for j in range(9):
+                if(self.matrix[i, j] == 0):
+                    count += 1
+        return count
+
     def inRow (self, row, number):
         """Return -1 if not present, the column index otherwise"""
-        for j in range(self.MAX):
+        for j in range(9):
             if (self.matix[row,j] == number):
                 return j
         return -1
 
     def inColumn (self, col, number):
         """Return -1 if not present, the row index otherwise"""
-        for i in range(self.MAX):
+        for i in range(9):
             if(self.matrix[i,col] == number):
                 return i
         return -1
@@ -54,17 +72,104 @@ class Sudoku:
         return (-1,-1)
 
     def rowOneMissingFrame (self, row):
-        """Return 1 if only one frame is missing, 0 otherwise"""
+        """Return the column index of the frame if only one is missing, -1 otherwise"""
         count = 0
-        for j in range(self.MAX):
+        index = -1
+        for j in range(9):
             if(self.matrix[row,j] == 0):
                 count +=1
-        return count == 1
+                index = j
+        if(count == 1):
+            return index
+        else:
+            return -1
     
     def colOneMissingFrame (self, col):
-        """Return 1 if only one frame is missing, 0 otherwise"""
+        """Return the row index of the frame if only one is missing, -1 otherwise"""
         count = 0
-        for i in range(self.MAX):
+        index = -1
+        for i in range(9):
             if(self.matrix[i, col] == 0):
                 count +=1
-        return count == 1
+                index = i
+        if(count == 1):
+            return index
+        else:
+            return -1
+
+    def blockOneMissingFrame (self, row, col):
+        """Return the couple (row,col) index of the frame if only one is missing, (-1,-1) otherwise"""
+        count = 0
+        index = (-1,-1)
+        for i in range (row, row+3):
+            for j in range (col, col+3):
+                if(self.matrix[i, j] == 0):
+                    count += 1
+                    index = (i, j)
+        if(count == 1):
+            return index
+        else:
+            return -1
+
+    # =======================================================================================
+    # Validity Functions ====================================================================
+    # =======================================================================================
+
+    def validMatrix (self):
+        """Return true if the matrix is valid, false otherwise"""
+        for i in range(9):
+            if(not validRow(i)):
+                return False
+        for j in range(9):
+            if(not validColumn(j)):
+                return False
+        for i in range(3):
+            for j in range(3):
+                if(not validBlock(i*3, j*3)):
+                    return False
+        return True
+
+    def validRow (self, row):
+        """Return true if the row is valid, false otherwise"""
+        for k in range(1, 10):
+            count = 0
+            for j in range(9):
+                if(self.matrix[row, j] == k):
+                    count += 1
+            if(count > 1):
+                return False
+        return True
+    
+    def validColumn (self, col):
+        """Return true if the column is valid, false otherwise"""
+        for k in range(1, 10):
+            count = 0
+            for i in range(9):
+                if(self.matrix[i, col] == k):
+                    count += 1
+            if(count > 1):
+                return False
+        return True
+
+    def validBlock (self, row, col):
+        """Return true if the bloc is valid, false otherwise"""
+        for k in range(1, 10):
+            count = 0
+            for i in range(3):
+                for j in range(3):
+                    if(self.matrix[row + i, col + j] == k):
+                        count += 1
+            if(count > 1):
+                return False
+        return True
+
+    # =======================================================================================
+    # Strategies Functions ==================================================================
+    # =======================================================================================
+
+    #def OneEmptyFrameStrategy (self):
+        
+    #def TwoEmptyFrameStrategyMoveOne (self):
+        
+    #def TwoEmptyFrameStrategyMoveTwo (self):
+        
